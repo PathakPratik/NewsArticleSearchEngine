cf933da6cb968588848a4686684ad2e4f4ae4c78
jackgilbride999 <jackgilbride999@gmail.com>
2021-11-05 14:14:53 +0000

    Index the DOCID, HEADLINE and TEXT fields of Financial Times Limited

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # NewsArticleSearchEngine
 IR Group Project
+
+## Project Structure
+
+For the indexing to work, topics should be inside a directory called resources. The dataset should be inside a directory called newsarticles inside resources. I.e:
+
++-- src/main/java
+|   +-- *source code files*
++-- resources
+|   +-- topics
+|   +-- newsarticles
+|   |   +-- Assignment Two
+|   |   |   +-- dtds
+|   |   |   +-- fbis
+|   |   |   +-- fr94
+|   |   |   +-- ft
+|   |   |   +-- latimes
+|   |   +-- _MACOSX
+|   |   |   +-- Assignment Two
+|   |   |   |   +-- dtds
+|   |   |   |   +-- fbis
+|   |   |   |   +-- fr94
+|   |   |   |   +-- ft
+|   |   |   |   +-- latimes
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,21 +8,36 @@ import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.NoSuchFileException;
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Parser
-{
-    //<! constant for an empty string
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class Parser {
+    // <! constant for an empty string
     private final String cEMPTY_STRING = "";
-    //<! identifier for the analyzer that is to be created from AnalyzerSimilarityFactory
+    // <! identifier for the analyzer that is to be created from
+    // AnalyzerSimilarityFactory
     private String mAnalyzerString;
-    //<! identifier for the similarity that is to be created from AnalyzerSimilarityFactory
+    // <! identifier for the similarity that is to be created from
+    // AnalyzerSimilarityFactory
     private String mSimilarityString;
 
     Parser(String analyzer, String similarity) {
@@ -31,14 +46,15 @@ public class Parser
     }
 
     /**
-        Extracts a substring from a string based on a regular expression
-
-        @param input the input string
-        @param regex the regular expression
-        @return the extracted substring. Return an empty string, if the regex did not match
+     * Extracts a substring from a string based on a regular expression
+     * 
+     * @param input the input string
+     * @param regex the regular expression
+     * @return the extracted substring. Return an empty string, if the regex did not
+     *         match
      */
     private String extractPattern(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex,Pattern.DOTALL);
+        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
             return matcher.group(0);
@@ -47,15 +63,21 @@ public class Parser
     }
 
     /**
-        This method extracts all the relevant info from the cran dataset that we need for
-        creating the index. This method creates the index after extraction
-
-        @param file location of the cran dataset
-        @param indexDirectoryLocation location where the created index should be stored
-        @return true if parsing was successful. Otherwise, false
+     * This method extracts all the relevant info from the cran dataset that we need
+     * for creating the index. This method creates the index after extraction
+     * 
+     * @param ftLocation             location of the Financial Times Limited dataset
+     * @param fr94Location           location of the Federal Register dataset
+     * @param fbisLocation           location of the Foreign Broadcast Information
+     *                               Service dataset
+     * @param latimesLocation        location of the Los Angeles Times dataset
+     * @param indexDirectoryLocation location where the created index should be
+     *                               stored
+     * @return true if parsing was successful. Otherwise, false
      */
-    public boolean createIndex(String file, String indexDirectoryLocation) throws IOException {
-        ArrayList<Document> documents = new ArrayList<>();
+    public boolean createIndex(String ftLocation, String fr94Location, String fbisLocation, String latimesLocation,
+            String indexDirectoryLocation) throws IOException {
+        System.out.println(Paths.get(indexDirectoryLocation).toAbsolutePath());
         Directory directory = FSDirectory.open(Paths.get(indexDirectoryLocation));
 
         // Set up an index writer to add process and save documents to the index
@@ -64,95 +86,110 @@ public class Parser
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         IndexWriter indexWriter = new IndexWriter(directory, config);
 
-        try {
-            String content = new String(Files.readAllBytes(Paths.get(file)));
-            System.out.printf("Indexing \"%s\"\n", file);
-            //split the document into the single entries
-            String[]entries = content.split("(?=.I \\d{1,4}\\n)");
-            for (String currEntry : entries)
-            {
-                //create a new document and add the respective fields
-                Document doc = new Document();
-                boolean empty = true;
+        boolean successful = indexFt(ftLocation, indexWriter);
 
-                //extract id
-                String id = extractPattern(currEntry,"(?<=.I )\\d{1,4}");
-                if(!id.isEmpty()) {
-                    doc.add(new TextField(FieldNames.ID.getName(), id, Field.Store.YES));
-                }
-
-                //extract title
-                String title = extractPattern(currEntry,"(?<=.T\\n).*?(?=.A\\n)");
-                if(!title.isEmpty()) {
-                    doc.add(new TextField(FieldNames.TITLE.getName(), title, Field.Store.YES));
-                    empty = false;
-                }
-
-                //extract author
-                String authors = extractPattern(currEntry,"(?<=.A\\n).*?(?=.B\\n)");
-                if(!authors.isEmpty()) {
-                    doc.add(new TextField(FieldNames.AUTHOR.getName(), authors, Field.Store.YES));
-                    empty = false;
-                }
-
-                //extract bibliography
-                String bibliography = extractPattern(currEntry,"(?<=.B\\n).*?(?=.W\\n)");
-                if(!bibliography.isEmpty()) {
-                    doc.add(new TextField(FieldNames.BIBLIOGRAPHY.getName(), bibliography, Field.Store.YES));
-                    empty = false;
-                }
-
-                //extract description
-                String description = extractPattern(currEntry,"(?<=.W\\n).*");
-                if(!description.isEmpty()) {
-                    doc.add(new TextField(FieldNames.DESCRIPTION.getName(), description, Field.Store.NO));
-                    empty = false;
-                }
-                //if the document contained nothing but an id -> do not add it to the index
-                if(!empty)
-                {
-                    documents.add(doc);
-                }
-            }
-        }
-        catch (InvalidPathException | NoSuchFileException exception){
-            System.out.println("Invalid path! Can not index: " + file);
-            indexWriter.close();
-            directory.close();
-            return false;
-        }
-    
-        //add the created documents to the index and close everything
-        indexWriter.addDocuments(documents);
+        // add the created documents to the index and close everything
         indexWriter.close();
         directory.close();
         System.out.println("Indexing finished successfully");
-        return true;
+        return successful;
     }
 
     /**
-     * This method extracts the queries from the cran.qry file and writes them
-     * to a <Integer,String> map (id of the query is mapped to the search text).
+     * This method extracts all the relevant info from the Financial Times Limited 
+     * dataset that we need for creating the index. This method creates the index 
+     * after extraction
+     * 
+     * @param ftLocation             location of the Financial Times Limited dataset
+     * @param indexWriter            the index writer used to create the index
+     * @return true if parsing was successful. Otherwise, false
+     */
+    private boolean indexFt(String ftLocation, IndexWriter indexWriter) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            org.w3c.dom.Document xmlDoc;
+            Enumeration<InputStream> stream;
+            SequenceInputStream sequenceStream;
+
+            File dir = new File(ftLocation);
+            for (File nestedDir : dir.listFiles()) {
+                for (File file : nestedDir.listFiles()) {
+                    stream = Collections.enumeration(
+                            Arrays.asList(new InputStream[] { new ByteArrayInputStream("<root>".getBytes()),
+                                    new FileInputStream(file), new ByteArrayInputStream("</root>".getBytes()), }));
+                    sequenceStream = new SequenceInputStream(stream);
+                    xmlDoc = db.parse(sequenceStream);
+
+                    xmlDoc.getDocumentElement().normalize();
+                    NodeList nodeList = xmlDoc.getElementsByTagName("DOC");
+
+                    ArrayList<Document> documents = new ArrayList<>();
+
+                    for (int i = 0; i < nodeList.getLength(); i++) {
+                        Node node = nodeList.item(i);
+                        System.out.println("\nNode Name:" + node.getNodeName());
+                        Element eElement = (Element) node;
+
+                        Document luceneDoc = new Document();
+                        boolean empty = true;
+
+                        String docNo = eElement.getElementsByTagName("DOCNO").item(0).getTextContent();
+                        System.out.println(docNo);
+                        if (!docNo.isEmpty()) {
+                            luceneDoc.add(new TextField(FieldNames.DOCNO.getName(), docNo, Field.Store.YES));
+                        }
+
+                        String headline = eElement.getElementsByTagName("HEADLINE").item(0).getTextContent();
+                        System.out.println(headline);
+                        if (!headline.isEmpty()) {
+                            luceneDoc.add(new TextField(FieldNames.HEADLINE.getName(), headline, Field.Store.YES));
+                            empty = false;
+                        }
+
+                        String text = eElement.getElementsByTagName("TEXT").item(0).getTextContent();
+                        System.out.println(text);
+                        if (!text.isEmpty()) {
+                            luceneDoc.add(new TextField(FieldNames.TEXT.getName(), text, Field.Store.YES));
+                            empty = false;
+                        }
+
+                        if (!empty) {
+                            documents.add(luceneDoc);
+                        }
+                    }
+
+                    indexWriter.addDocuments(documents);
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * This method extracts the queries from the cran.qry file and writes them to a
+     * <Integer,String> map (id of the query is mapped to the search text).
      *
      * @param queryFileLocation the location of the file which contains the queries
      * @return a map <Integer,String> which maps id of a query to its search text
      * @throws IOException if the cran query file could not be read in
      */
-    public HashMap<Integer,String> createQueries(String queryFileLocation) throws IOException {
+    public HashMap<Integer, String> createQueries(String queryFileLocation) throws IOException {
         System.out.println("Started extracting queries");
-        HashMap<Integer,String> queryMap = new HashMap<>();
+        HashMap<Integer, String> queryMap = new HashMap<>();
         String content = new String(Files.readAllBytes(Paths.get(queryFileLocation)));
 
-        //split the document into the single queries
-        String[]queries = content.split("(?=.I \\d{1,4}\\n)");
+        // split the document into the single queries
+        String[] queries = content.split("(?=.I \\d{1,4}\\n)");
         int id = 1;
-        for (String currQuery : queries)
-        {
-            //extract the query text
-            String queryText = extractPattern(currQuery,"(?<=.W\\n).*");
+        for (String currQuery : queries) {
+            // extract the query text
+            String queryText = extractPattern(currQuery, "(?<=.W\\n).*");
 
-            if(!queryText.isEmpty()) {
-                queryMap.put(id,queryText);
+            if (!queryText.isEmpty()) {
+                queryMap.put(id, queryText);
             }
             id++;
         }
