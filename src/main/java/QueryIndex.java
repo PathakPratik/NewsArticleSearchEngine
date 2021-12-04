c4d1ce9785f443af8fba85e0c37406054307d45b
PathakPratik <rajpratik10@gmail.com>
2021-12-04 18:17:48 +0000

    changed limit for high freq words

--- src/main/java/QueryIndex.java
+++ src/main/java/QueryIndex.java
@@ -90,7 +90,7 @@ public class QueryIndex {
         ArrayList<String> stopWordlist = new ArrayList<String>();
 
         for (org.apache.lucene.misc.TermStats commonTerm : commonTerms) {
-            if( commonTerm.totalTermFreq > 1800000 ) {
+            if( commonTerm.totalTermFreq > 200000 ) {
                 stopWordlist.add(commonTerm.termtext.utf8ToString());
             }
         }
